536a1361e84aa68c6bfdba85f845cc42a7bd89f0
Cat Yanish <catyanish@gmail.com>
2017-07-01 14:01:38 -0500

    completed base mode

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,44 @@
+var x;
+var y;
+var operator;
 console.log('JS sourced');
 $(document).ready(function () {
 console.log('document ready');
 
+//need to set operator value when a operator button is clicked.
+//too tired to think of efficient method for this.
+$(".operator-button").on('click', function() {
+  operator = $(this).attr('id');
+  console.log(operator);
+}); //end of on click for operator buttons
 
 
+
+$('#submit').on('click', function() {
+  console.log($('#x').val());//this will send as a string
+  console.log($('#y').val());
+//on click of Calculate button, ajax post this
+  $.ajax({
+    type: 'POST',
+    url: '/calculate',
+    data: { x: $('#x').val(), //x: the value of first input
+            y: $('#y').val(), //y: the value of second input
+            type: operator}, //NOT SURE HOW SERVER WILL READ THIS....
+            //it IS part of an object, so should be able to access that
+            //on server and then call the appropriate function using a switch statement.
+    success: function(response) {
+      console.log('posted some data');
+      //want to append the response to the DOM
+      $('.answer').append(response);
+    }
+  }); //end of AJAX post function.
+
+}); //end of on click function
+
+$("#reset").on('click', function () {
+  $('input').val('');
+  $('.answer').empty();
+}); //end of reset on click function
 
 
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,50 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 var path = require("path");
+var response; //NOT SURE IF THIS GOES HERE
+
+app.post('/calculate', function(req, res){
+  var type = req.body.type;
+  //the req.body function would do nothing without body parser.
+  var x = parseInt(req.body.x);
+  var y = parseInt(req.body.y);
+  console.log(x);
+  console.log(y);
+  console.log(type);
+  switch(type) {
+    case "add":
+      answer = x + y;
+      console.log(answer);
+      response = answer.toString();
+      console.log(response);
+      res.send(response);
+      break;
+    case "subtract":
+      answer = x - y;
+      response = answer.toString();
+      console.log(response);
+      res.send(response);
+      break;
+    case "multiply":
+      answer = x * y;
+      response = answer.toString();
+      console.log(response);
+      res.send(response);
+      break;
+    case "divide":
+      answer = x / y;
+      response = answer.toString();
+      console.log(response);
+      res.send(response);
+      break;
+  } //end of switch statement
+
+  //here we add the information from the client request to our existing array.
+   //not SURE IF SCOPING PROBLEMS
+  //respond to the request with a success status message.
+});
+
+
 
 
 app.get("/*", function(req, res) {
