bcec8a3633d48c78abc8daf091d1a5db27575888
Cat Yanish <catyanish@gmail.com>
2017-07-02 21:19:54 -0500

    refactor code and begin add router

--- server/public/scripts/client2.js
+++ server/public/scripts/client2.js
@@ -1,60 +1,65 @@
 var xvalue;
 var yvalue;
 var operator = null;
-console.log('JS sourced');
+
 $(document).ready(function () {
-console.log('document ready');
-
-$(".operator-button").on('click', function() {
-  $('#display-window').empty();
-  operator = $(this).attr('id');
-  console.log(operator);
-});//end of operator on click button function
-
-
-$(".number-button").on('click', function() {
-  if(operator == null) {
-        $('#display-window').append($(this).attr('id'));
-        xvalue = $('#display-window').text();
-        console.log(xvalue);
-  } else {
-    $('#display-window').append($(this).attr('id'));
-     yvalue = $('#display-window').text();
-     console.log(yvalue);
-  }
-}); //end of click function
-
-
-
-$('#submit').on('click', function() {
-  console.log(xvalue);//this will send as a string
-  console.log(yvalue);
-//on click of Calculate button, ajax post this
-  $.ajax({
-    type: 'POST',
-    url: '/calculate',
-    data: { x: xvalue, //x: the value of first input
-            y: yvalue, //y: the value of second input
-            type: operator}, //NOT SURE HOW SERVER WILL READ THIS....
-            //it IS part of an object, so should be able to access that
-            //on server and then call the appropriate function using a switch statement.
-    success: function(response) {
-      console.log('posted some data');
-      $('#display-window').text('Computing...'); //changed from .answer
-      //want to append the response to the DOM
-      setTimeout(function(){
-        $('#display-window').text(response);  //changed from .answer
-      }, 3000);
-      // $('.answer').append(response);
-    }
-  }); //end of AJAX post function.
-
-}); //end of on click function
-
-$("#reset").on('click', function () {
-  $('#display-window').text('');
-}); //end of reset on click function
-
-
-
+  numberClickEvents();
+  submitPost();
+  operatorClick();
+  resetButton();
 }); //end of doc ready
+
+//math operator click
+function operatorClick() {
+  $(".operator-button").on('click', function() {
+    $('#display-window').empty();
+    operator = $(this).attr('id');
+  });//end of on click
+}//end of operatorClick function
+
+
+//if operator button hasn't been clicked, input concatanates to x
+//after operator button click, input concats to y value.
+function numberClickEvents() {
+  $(".number-button").on('click', function() {
+    if(operator == null) {
+      $('#display-window').append($(this).attr('id'));
+      xvalue = $('#display-window').text();
+    } else {
+      $('#display-window').append($(this).attr('id'));
+       yvalue = $('#display-window').text();
+    }
+  }); //end of click function
+} //end of numberClickEvents function
+
+
+//submit button posts x and y values and operator type to server
+function submitPost() {
+  $('#submit').on('click', function() {
+    $.ajax({
+      type: 'POST',
+      url: '/calculate',
+      data: { x: xvalue, //x: the value of first input
+              y: yvalue, //y: value of second input
+              type: operator},
+      //response is set to delay 3 seconds with setTimeout function
+      success: function(response) {
+        $('#display-window').text('Computing...');
+        setTimeout(function(){
+          $('#display-window').text(response);
+        }, 3000); //end of setTimeout function
+      } //success function
+    }); //AJAX post function.
+  }); //on click function
+}//end of submitPost
+
+
+//reset button clears display and resets x and y to zero
+function resetButton() {
+  $("#reset").on('click', function () {
+    xvalue = 0;
+    yvalue = 0;
+    operator = null;
+    $('#display-window').empty();
+  });
+} //reset on click function
--- server/server.js
+++ server/server.js
@@ -3,47 +3,41 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 var path = require("path");
-var response; //NOT SURE IF THIS GOES HERE
+var response;
+
+
+// var productRouter = require('./calcRouter/calc.js');
+// app.use('/product', productRouter);
+// //app use takes /PRODUCT ALL REQUESTS and SEND IT TO ROUTER!
+//
+
 
 app.post('/calculate', function(req, res){
   var type = req.body.type;
-  //the req.body function would do nothing without body parser.
   var x = parseInt(req.body.x);
   var y = parseInt(req.body.y);
-  console.log(x);
-  console.log(y);
-  console.log(type);
   switch(type) {
     case "add":
       answer = x + y;
-      console.log(answer);
       response = answer.toString();
-      console.log(response);
       res.send(response);
       break;
     case "subtract":
       answer = x - y;
       response = answer.toString();
-      console.log(response);
       res.send(response);
       break;
     case "multiply":
       answer = x * y;
       response = answer.toString();
-      console.log(response);
       res.send(response);
       break;
     case "divide":
       answer = x / y;
       response = answer.toString();
-      console.log(response);
       res.send(response);
       break;
   } //end of switch statement
-
-  //here we add the information from the client request to our existing array.
-   //not SURE IF SCOPING PROBLEMS
-  //respond to the request with a success status message.
 });
 
 
